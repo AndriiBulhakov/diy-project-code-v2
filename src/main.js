ff4dc843e77573e05bb29a2c2c67d6d40c4ca15f
Andrii Bulhakov <andriibulhakov@Andriis-MacBook-Pro.local>
2023-08-23 20:42:20 +0200

    main fixed

--- src/main.js
+++ src/main.js
@@ -2,11 +2,7 @@
 import callApi from './scripts/wizard-component'
 import createMap from './scripts/wizard-map'
 import initModel from './scripts/wizard-model'
-// import controlsSetup from './scripts/wizard-controls'
-import preloader from './scripts/wizard-preloader'
 
 createMap()
 callApi()
 initModel()
-// controlsSetup()
-// window.addEventListener('load', preloader)
